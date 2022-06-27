90befd085ebbd78e254293f9c58b0e8f51998777
ZAHID188 <zahidpranta5@gmail.com>
2022-06-27 15:44:53 +0600

    change

---
+++ dist/_nuxt/static/1656321214/admin/new-post/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002FBlog_live\u002F_nuxt\u002Fstatic\u002F1656321214",layout:"default",error:a,serverRendered:true,routePath:"\u002Fadmin\u002Fnew-post",config:{_app:{basePath:"\u002FBlog_live\u002F",assetsPath:"\u002FBlog_live\u002F_nuxt\u002F",cdnURL:a}}}}(null));
